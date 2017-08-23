2c8f16b512bb73ee61a8ff73d5f04a99a91d3dd8
Yogesh Luthra <yogeshluthra@gmail.com>
2017-08-23 16:58:16 -0700

    pull request 2A-L2

--- 2A-L2/answers/remove_noise.py
+++ 2A-L2/answers/remove_noise.py
@@ -1,42 +1,47 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
+# Helper functions
+def normalize_image(image):
+    """normalize the image between 0-255 and setting data type=uint8"""
+    minImg, maxImg = np.min(image)*1., np.max(image)*1.
+    return (((image - minImg)/(maxImg-minImg))*255).astype(np.uint8)
 
-# Helper function
 def imshow(title, image):
-    # WARNING: Using np.clip to get results similar to the quiz.
-    # This is NOT a good practice because you are discarding information that is
-    # below 0 and above 255. Use normalization instead.
-
-    image = np.clip(image, 0, 255).astype(np.uint8)
-    cv2.imshow(title, image.astype(np.uint8))
+    """normalize the image between 0-255 and then show"""
+    print 'To show, first normalizing the image between 0-255 and then setting data type=uint8'
+    image = normalize_image(image)
+    cv2.imshow(title, image)
 
 # Apply a Gaussian filter to remove noise
-img = cv2.imread('../images/saturn.png', 0)
-cv2.imshow('Img', img)
+img = cv2.imread('images/saturn.png', 0) # Read image as grayscale (because of 0. for color, use 1. for unchanged, use -1)
+imshow('Img_original', img)
+X, Y = map(np.arange, img.shape)
+X, Y = np.meshgrid(X, Y, indexing='ij')
 
-# Add some noise
-noise_sigma = 25
-r, c = img.shape
-noise = np.random.randn(r, c) * noise_sigma
-noisy_img = img + noise
+# TODO: Add noise to the image
+noise_sigma = 25 # noise standard deviation
+noise = (np.random.randn(*img.shape))*noise_sigma
 
-imshow('Noisy Image', noisy_img)  # Customized imshow function to match the quiz
+img_noisy = img + noise
+imshow('Img_noisy',img_noisy)
 
-# Create a Gaussian filter
-filter_size = 11
-filter_sigma = 2
+# TODO: Now apply a Gaussian filter to smooth out the noise
+gauss_filter = cv2.getGaussianKernel(11,2) # this is just 1D gaussian with spacial sigma of 2 cells.
+print gauss_filter
+img_getGaussianKernel = cv2.sepFilter2D(img_noisy, -1, gauss_filter, gauss_filter) # desired depth=-1 (sets depth (or dtype) of new image same as src image)
+imshow('Img_getGaussianKernel', img_getGaussianKernel)
 
-# OpenCV does not have a fspecial type of function. Instead we will cv2.getGaussianKernel
-# to generate a Gaussian kernel.
-filter_kernel = cv2.getGaussianKernel(filter_size, filter_sigma)
+# TODO: Using GaussianBlur
+img_GaussianBlur = cv2.GaussianBlur(img_noisy,(11,11),2)
+imshow('Img_GaussianBlur', img_GaussianBlur)
 
-# This however generates a 1D kernel and we want a 2D that is of shape (filter_size, filter_size)
-# To achieve this we multiply this array by its transpose
-filter_kernel = filter_kernel * filter_kernel.T
+# fig=plt.figure()
+# ax=Axes3D(fig)
+# ax.plot_surface(X, Y, noise)
+# plt.show()
 
-# Apply it to remove noise
-smoothed = cv2.filter2D(noisy_img, -1, filter_kernel)
-
-imshow('Smoothed image', smoothed)  # Customized imshow function to match the quiz
 cv2.waitKey(0)
+cv2.destroyAllWindows()
