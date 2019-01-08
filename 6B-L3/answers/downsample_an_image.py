832737a49897a3ebb71a7b91453878166e80b72b
Pedro <pdvelez@gmail.com>
2019-01-07 23:06:41 -0500

    Reformat code

--- 6B-L3/answers/downsample_an_image.py
+++ 6B-L3/answers/downsample_an_image.py
@@ -1,28 +1,29 @@
 import cv2
 
+
 def downsample(img):
-	#TODO: img_d = ? (pick alternate rows, cols: 1, 3, 5, ...)
-	img_d = img[::2, ::2, :]
-	return img_d
+    # TODO: img_d = ? (pick alternate rows, cols: 1, 3, 5, ...)
+    img_d = img[::2, ::2, :]
+    return img_d
 
 
 def blur_downsample(img):
-	#TODO img_bd = ? (blur by 5x5 gaussian, then downsample)
-	img_bd = cv2.GaussianBlur(img, (5,5), 0, 0)[::2, ::2, :]
-	return img_bd
+    # TODO img_bd = ? (blur by 5x5 gaussian, then downsample)
+    img_bd = cv2.GaussianBlur(img, (5, 5), 0, 0)[::2, ::2, :]
+    return img_bd
 
 
 img = cv2.imread('../images/frizzy.png')
 cv2.imshow("original_image", img)
 print(img.shape)
 
-#downsample image
+# downsample image
 img_d = downsample(img)
 img_d = downsample(img_d)
 img_d = downsample(img_d)
 print(img_d.shape)
 
-#blur and downsample
+# blur and downsample
 img_bd = blur_downsample(img)
 img_bd = blur_downsample(img_bd)
 img_bd = blur_downsample(img_bd)
@@ -31,7 +32,3 @@ print(img_bd.shape)
 cv2.imshow("downsampled_image", cv2.resize(img_d, (img.shape[1], img.shape[0])))
 cv2.imshow("blur_downsampled_image", cv2.resize(img_bd, (img.shape[1], img.shape[0])))
 cv2.waitKey(0)
-
-
-
-
--- 6B-L3/downsample_an_image.py
+++ 6B-L3/downsample_an_image.py
@@ -1,26 +1,27 @@
 import cv2
 
+
 def downsample(img):
-	#TODO: img_d = ? (pick alternate rows, cols: 1, 3, 5, ...)
-	pass
+    # TODO: img_d = ? (pick alternate rows, cols: 1, 3, 5, ...)
+    pass
 
 
 def blur_downsample(img):
-	#TODO img_bd = ? (blur by 5x5 gaussian, then downsample)
-	pass
+    # TODO img_bd = ? (blur by 5x5 gaussian, then downsample)
+    pass
 
 
 img = cv2.imread('images/frizzy.png')
 cv2.imshow("original_image", img)
 print(img.shape)
 
-#downsample image
+# downsample image
 img_d = downsample(img)
 img_d = downsample(img_d)
 img_d = downsample(img_d)
 print(img_d.shape)
 
-#blur and downsample
+# blur and downsample
 img_bd = blur_downsample(img)
 img_bd = blur_downsample(img_bd)
 img_bd = blur_downsample(img_bd)
@@ -29,5 +30,3 @@ print(img_bd.shape)
 cv2.imshow("downsampled_image", cv2.resize(img_d, (img.shape[1], img.shape[0])))
 cv2.imshow("blur_downsampled_image", cv2.resize(img_bd, (img.shape[1], img.shape[0])))
 cv2.waitKey(0)
-
-
